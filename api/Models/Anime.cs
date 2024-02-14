1e00f5a4d23d979a5fa22663146d802534519a8a
FurkanOzkan <furkanozkann01@gmail.com>
2024-02-14 18:36:58 +0300

    dev: added models added nuget readme image

---
+++ api/Models/Anime.cs
@@ -0,0 +1,9 @@
+namespace api.Models
+{
+    public class Anime
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool status { get; set; }
+    }
+}
---
+++ api/Models/User.cs
@@ -0,0 +1,11 @@
+namespace api.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public List<Anime> watchedList { get; set; } = new List<Anime>();
+        public List<Anime> toWatchList { get; set; } = new List<Anime>();
+    }
+}
