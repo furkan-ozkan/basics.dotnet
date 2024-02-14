4b67a2c9ab2fa55087646ea456ea04f1a12e6e05
FurkanOzkan <furkanozkann01@gmail.com>
2024-02-14 18:59:51 +0300

    dev: added dbcontext added readme images

---
+++ api/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Anime> Animes { get; set; }
+    }
+}
